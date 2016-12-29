a6853c212cef9fd271edbaf1ce8553d037db3e71
Igor Trofimov <igor@roomorama.com>
2016-12-29 09:12:39 +0800

    init commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>ЗА ОКНОМ</h2>
+        </div>
+        <p className="App-intro">
+          Блог <a href="">Ирины Мироновой</a> должен в скором времени появиться
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
